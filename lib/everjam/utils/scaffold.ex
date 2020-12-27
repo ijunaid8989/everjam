078d32b200c953a464838757bd306323fbb4f10a
Junaid Farooq <junaid@evercam.io>
2020-12-27 09:16:46 +0500

    Added Scaffold for Camera && User.

---
+++ lib/everjam/utils/scaffold.ex
@@ -0,0 +1,30 @@
+defmodule Scaffold do
+  @moduledoc false
+
+  defmacro __using__(opts) do
+    quote generated: true, location: :keep do
+      use GenServer
+
+      defmodule Attributes, do: defstruct unquote(opts)
+
+      def start_link(state \\ %Attributes{}) do
+        GenServer.start_link(__MODULE__,
+          %Attributes{state | id: make_ref()})
+      end
+
+      def init(state), do: {:ok, state}
+
+      def details(pid),
+        do: GenServer.call(pid, :get)
+
+      def update(pid, map),
+        do: GenServer.cast(pid, {:update, map})
+
+      def handle_call(:get, _from, state),
+        do: {:reply, state, state}
+
+      def handle_cast({:update, map}, state),
+        do: {:noreply, Map.merge(map, state)}
+    end
+  end
+end
