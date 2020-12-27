d59deb3092fa17aa87b32c9b934edbea96f3949d
Junaid Farooq <junaid@evercam.io>
2020-12-27 09:17:22 +0500

    Added CamBank.

---
+++ lib/everjam/utils/cam_bank.ex
@@ -0,0 +1,27 @@
+defmodule CamBank do
+  use GenServer
+
+  def start_link(state \\ [] ) do
+    GenServer.start_link(__MODULE__, state)
+  end
+
+  def init(state) do
+    {:ok, state}
+  end
+
+  def all(pid) do
+    GenServer.call(pid, :get)
+  end
+
+  def add(pid, map) do
+    GenServer.cast(pid, {:add, map})
+  end
+
+  def handle_call(:get, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_cast({:add, pid}, state) do
+    {:noreply, [pid | state]}
+  end
+end
