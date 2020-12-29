daa5f5a3e502d31ab35ff4ea183bf7fbaa353158
Junaid Farooq <junaid@evercam.io>
2020-12-29 22:37:47 +0500

    Added General Supervisor.

---
+++ lib/everjam/utils/general_supervisor.ex
@@ -0,0 +1,14 @@
+defmodule General.Supervisor do
+  use DynamicSupervisor
+
+  def start_link(opts) do
+    DynamicSupervisor.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  def start_child(module, opts),
+    do: DynamicSupervisor.start_child(__MODULE__, {module, opts})
+
+  @impl DynamicSupervisor
+  def init(opts),
+    do: DynamicSupervisor.init(strategy: :one_for_one, extra_arguments: [opts])
+end
