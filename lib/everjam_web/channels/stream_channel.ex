1ebc4164afcd1205e516398744112c19891ceea5
Junaid Farooq <junaid@evercam.io>
2021-01-03 21:56:20 +0500

    Start streamer when there is camera && User joined.

--- lib/everjam_web/channels/stream_channel.ex
+++ lib/everjam_web/channels/stream_channel.ex
@@ -10,8 +10,13 @@ defmodule EverjamWeb.StreamChannel do
 
   @impl true
   def handle_info({:after_join, camera_name}, socket) do
-    # start streaming
-    IO.inspect(camera_name)
+    Everjam.get_camera_from_bank(camera_name)
+    |> should_start()
     {:noreply, socket}
+  end
+
+  defp should_start(nil), do: :noop
+  defp should_start(camera) do
+    General.Supervisor.start_child(Streamer.StreamStarter, %{id: String.to_atom("#{camera.name <> "_streamer"}"), camera: camera, sleep: 1000, streaming: true}) |> IO.inspect()
   end
 end
