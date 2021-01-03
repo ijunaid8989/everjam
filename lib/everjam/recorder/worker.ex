0d23772aae9fef00321b8e0ff6381698f1c341b7
Junaid Farooq <junaid@evercam.io>
2021-01-03 21:52:51 +0500

    Broadcast stream as well as save to weed && Termination assurance.

--- lib/everjam/recorder/worker.ex
+++ lib/everjam/recorder/worker.ex
@@ -1,5 +1,6 @@
 defmodule Recording.Worker do
   use GenServer, restart: :transient
+  require Logger
 
   def start_link(state),
     do: GenServer.start_link(__MODULE__, state)
@@ -12,12 +13,23 @@ defmodule Recording.Worker do
   def handle_info(:fetch_and_process, state) do
     case make_jpeg_request(state.camera, state.running.datetime) do
       {:failed, _requested_at} ->
-        {:noreply, state}
-      {body, file_size, requested_at} ->
-        %{datetime: requested_at, image: body, file_size: file_size}
+        ConCache.put(:do_camera_request, state.camera.name, true)
+        {:stop, :shutdown, state}
+      {body, requested_at} ->
+        %{datetime: requested_at}
         |> put_it_in_jpeg_bank(state.camera.name)
+        requested_at
+        |> Calendar.strftime("#{state.camera.name}/snapshots/%Y/%m/%d/%H_%M_%S.jpg")
+        |> Everjamer.post("http://localhost:8888/", body)
+        Broadcasting.any_one_wacthing?(state.camera.name)
+        |> Broadcasting.stream(state.camera.name, body, requested_at)
+        ConCache.put(:do_camera_request, state.camera.name, true)
         {:stop, :normal, state}
     end
+  end
+
+  def terminate(me, _state) do
+    Logger.debug("I am terminated as #{me}")
   end
 
   defp make_jpeg_request(camera, requested_at) do
@@ -26,8 +38,8 @@ defmodule Recording.Worker do
     |> get_body_size(requested_at)
   end
 
-  defp get_body_size({:ok, %Finch.Response{body: body, headers: _headers, status: 200}}, requested_at) do
-    {body, "9", requested_at}
+  defp get_body_size({:ok, %Finch.Response{body: body, status: 200}}, requested_at) do
+    {body, requested_at}
   end
 
   defp get_body_size(_error, requested_at), do: {:failed, requested_at}
